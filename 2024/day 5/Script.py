c6d100ab56ca1566fadf15c46af21857e0a69f78
Tim Odermatt <tim.odermatt2005@gmail.com>
2024-12-05 22:31:09 +0100

    Finished Day 5 part 2

--- 2024/day 5/Script.py
+++ 2024/day 5/Script.py
@@ -6,6 +6,7 @@ part1 = 0
 part2 = 0
 output1 = []
 output2 = []
+# w stays undefined
 # x stays undefined
 # y stays undefined
 # z stays undefined
@@ -14,7 +15,24 @@ output2 = []
 INPUT_STRING = open("puzzle_input.txt", "r").read().split()
 
 
+def ordercheck(z, order, rules):
+    if order[z+1] in rules[order[z]]:
+        return True
+    else:
+        return False
+
+def completecheck(order, rules):
+    for z in range(len(order)-1):
+        if ordercheck(z, order, rules):
+            pass
+        else:
+            return False
+    else:
+        return True
+
+
 rulesAfter = {}
+rulesBevor = {}
 x = 0
 while len(INPUT_STRING[x]) < 6:
     rule = INPUT_STRING[x][0]+INPUT_STRING[x][1]
@@ -24,17 +42,25 @@ while len(INPUT_STRING[x]) < 6:
     else:
         rulesAfter.update({rule:[]})
         rulesAfter[rule].append(after)
+    if after in rulesBevor:
+        rulesBevor[after].append(rule)
+    else:
+        rulesBevor.update({after:[]})
+        rulesBevor[after].append(rule)
 
     x += 1
 
 for y in range(len(INPUT_STRING)-x): #len(INPUT_STRING)-x
     ordering = INPUT_STRING[x+y].split(",")
     for z in range(len(ordering)-1):
-        if ordering[z+1] in rulesAfter[ordering[z]]:
-            #print(f"{ordering[z]} in {ordering[z+1]}")
+        if ordercheck(z, ordering, rulesAfter):
             pass
         else:
-            reordering = ordering
+            reordering = ordering[:]
+            while completecheck(reordering, rulesAfter) == False:
+                for w in range(1, len(reordering)):
+                    if not ordercheck(0, [reordering[w], (reordering[w-1])], rulesBevor):
+                        reordering[w], reordering[w-1] = reordering[w-1], reordering[w]
             output2.append(int(reordering[int((len(reordering)-1)/2)]))
             break
     else:
